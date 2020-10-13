506ad78af5b9a71550353d8289bceea8171d0a9e
Ardelon <akeskinw@gmail.com>
2020-10-13 22:29:43 +0300

    -

---
+++ cvData.js
@@ -0,0 +1,114 @@
+const data =  [
+    {
+        "title":"Personal Information",
+        "information": [
+            {"name":"Alihan Keskin"},
+            {"maritalStatus":"Single"}
+        ]
+    },
+    {
+        "title":"Contact",
+        "information":[
+            {"mail":"akeskinw@gmail.com"},
+            {"phone":"905384643518"},
+            {"github":"https://github.com/Ardelon"},
+            {"linkedin":"https://www.linkedin.com/in/alihankeskin/"},
+            {"address":"Istanbul"}
+        ]
+    },
+    {
+        "title":"Education",
+        "information":[
+            {
+                "name":"Istanbul Technical University",
+                "department":"Geomatics Engineering",
+                
+            }
+        ]
+    },
+    {
+        "title":"Job Experience",
+        "information": [
+            {
+                "name":"Vucos",
+                "active":"1 year",
+                "jobDescription":"loremPariatur excepteur esse et laboris eu minim reprehenderit nulla enim. Officia exercitation enim sunt magna. Esse fugiat commodo reprehenderit qui proident. Labore exercitation incididunt officia nisi exercitation do. Eiusmod deserunt nulla non tempor enim qui exercitation. Enim veniam id in dolor eiusmod nisi aliquip ea enim aute sit voluptate amet sit. Ex sit cillum laboris labore enim ad nisi."
+            }
+        ]
+    },
+    {
+        "title":"Skillset",
+        "information":[
+            "Javascript",
+            "HTML",
+            "CSS",
+            "Webpack",
+            "React",
+            "Redux",
+            "Jquery",
+            "Python",
+            "Jira",
+            "Node.js",
+            "Bootstrap",
+            "Chrome Extensions",
+            "Github",
+            "JSX",
+            "SASS",
+            "Lodash",
+            "Videojs",
+            "Bitbucket",
+            "Sourcetree",
+            "Postman API",
+            "VS Code",
+            "Restful API",
+            "WebOS",
+            "Tizen",
+            "ChromeCast"
+        ]
+    },
+    {
+        "title":"References",
+        "information":[
+            {
+                "name":"Ozan Ertürk",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Berkay Oruç",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Oğuz Serin",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Kurtman Çelik",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Ahmet Faruk Taştekin",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Onur Erdoğan",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Nur Emirlioğlu",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            },
+            {
+                "name":"Kübra Korkmaz",
+                "position":"Software Developer",
+                "contact":"asdasdas"
+            }
+        ]
+    }
+]
